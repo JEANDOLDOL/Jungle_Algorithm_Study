f38883cb96794044a9f27df50b1ab683c9ddaab6
John Jin <johnjin56@gmail.com>
2024-08-19 10:25:35 +0900

    commit monday

---
+++ 0_Escape/JJW.py
@@ -0,0 +1,23 @@
+import collections
+
+R, C = map(int, input().split())
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+queue = collections.deque()
+
+
+def bfs(graph, x, y):
+
+    return
+
+
+graph = []
+
+for i in range(R):
+    graph += input().split()
+
+
+# test print
+print(graph[0][0])
