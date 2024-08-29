9cb9be18f3526e1c9516277aa392802c64935b88
897754 <42639194+897754@users.noreply.github.com>
2024-08-29 11:04:31 +0900

    Jihun

--- 0_TreeRadius/Jihun.py
+++ 0_TreeRadius/Jihun.py
@@ -3,38 +3,43 @@ import heapq
 
 sys.setrecursionlimit(10**8)
 
+MAX = 0
+
 class Node:
     def __init__(self) -> None:
         self.adjNodes = []
+        self.depth = 0
     def addAdj(self, target, length):
         self.adjNodes.append((target, length))
 
 def DFS(cur):
+    global MAX
+    h = [0,0]
     max = 0
-    for i in cur.adjNodes:
-        curDep = DFS(i)+cur[1]
+    for i in cur[0].adjNodes:
+        curDep = DFS(i)
+        heapq.heappush(h, -curDep)
         if (max < curDep):
             max = curDep
 
-    return max
-def Search(root:Node):
-    h = [0,0]
-    for i in root.adjNodes:
-        heapq.heappush(h, -DFS(i))
-    
     sum = 0
     sum -= heapq.heappop(h)
     sum -= heapq.heappop(h)
-    return sum
-
+    
+    if MAX < sum:
+        MAX = sum
+    cur[0].depth = max+cur[1]
+    return cur[0].depth
+    
 input = sys.stdin.readline
 
 N = int(input())
 
 nodes = [Node() for _ in range(N)]
 
-for _ in range(N):
+for _ in range(N-1):
     temp = input().split()
     nodes[int(temp[0])-1].addAdj(nodes[int(temp[1])-1], int(temp[2]))
-
-print(Search(nodes[0]))
+    
+DFS((nodes[0],0))
+print(MAX)
