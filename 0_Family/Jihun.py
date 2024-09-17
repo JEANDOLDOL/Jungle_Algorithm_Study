e5a87ee14ce8f1db182128d67924f6ae26692531
897754 <42639194+897754@users.noreply.github.com>
2024-09-17 13:34:42 +0000

    EditPath

---
+++ 0_Family/Jihun.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+temp = input().split()
+fr = int(temp[0])
+to = int(temp[1])
+M = int(input())
+
+for _ in range(M):
+    temp = input().split()
+    
