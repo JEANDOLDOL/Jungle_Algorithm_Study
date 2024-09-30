3f31475a448e7341bef8d941a4a317f9d9301446
JIN JAE WOONG <johnjin56@gmail.com>
2024-09-30 14:47:01 +0900

    aa

--- 0_LinearSum/jjw.py
+++ 0_LinearSum/jjw.py
@@ -5,7 +5,16 @@ N = int(input())
 graph = list(map(int, input().split()))
 
 dp = 0
+max_n = 0
 
-# for i in range(N+1):
-
+for i in range(N):
+    dp += graph[i]
+    if dp < 0:
+        dp = 0
+    if max_n < dp:
+        max_n = dp
+if max_n == 0:
+    max_n = max(graph)
+print(max_n)
+# print(dp)
 # helloo
